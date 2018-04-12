b86c999015ac09ab3dc40b9274f9ac462a2faa69
Vanessa Braglia <braglv@usi.ch>
2018-04-12 16:15:59 +0200

    Filter information. Ready to be used

--- nutchCrawler/take_names.py
+++ nutchCrawler/take_names.py
@@ -1,9 +1,10 @@
+# coding=utf-8
+
 # Lets use a regular expression to extract members' names
 # from the crawled html pages
 import re
 import os
 import json
-
 
 '''##############################
          HELPER FUNCTIONS
@@ -17,41 +18,82 @@ def to_plane_text(fileLine):
 
 # Take only the Switzerland researchers and same them temporarily in a file
 def split_lines(fileLine):
+   # 'University of Zürich and ETH Zürich'
+   # 'ETH Zürich and Paul Scherrer Institutee'
    components = fileLine.split(';')
    res = ""
    for x in components:
-	   if 'Switzerland' in x:
+      if 'Switzerland and ' in x:
+         res=x.split(' and ')[0]+';';
+         break
+      if 'University of Zurich and ETH Zurich' in x:
+         res = x.split(' and ETH')[0]+',Switzerland;'
+         res = res+x.split('University')[0]+'ETH Zürich,Switzerland;'
+         break
+      if 'ETH Zürich and Paul Scherrer Institute' in x:
+         res = x.split(' and ')[0]+',Switzerland;'
+         res = res+x.split(' ETH')[0]+' Paul Scherrer Institute,Switzerland;'
+         break
+      if 'Switzerland' in x:
 		   res = res + x + ';';
    if res != "":
       file_result.write(res+'\n')
    return
 
+# Helper function that adjust names of university
+def adjust_university(uni):
+   uni = uni.replace('Zurich', 'Zürich')
+   uni = uni.replace('Institut', 'Institute')
+   uni = uni.replace('Institutee', 'Institute')
+   uni = uni.replace('Berne', 'Bern')
+   uni = uni.replace('Bernee', 'Bern')
+   uni = uni.replace('Universität', 'University of')
+   uni = uni.replace('Université de Genève', 'University of Geneva')
+   uni = uni.replace('Philip Morris Int.','Philip Morris International R&D')
+   uni = uni.replace('IBM Research - Zurich','IBM Research')
+   uni = uni.replace('IBM Research - Zürich','IBM Research')
+   uni = uni.replace('Univeristy', 'University')
+   uni = uni.replace('Universite de', 'University of')
+   if 'polytechnique f' in uni:
+      uni = uni.replace('polytechnique f','Polytechnique F')
+   return uni
+
 # Extract names from pasc conferences
 def take_names_pasc(co_organisers):
-	for x in co_organisers:
-		if "Switzerland" in x:
-			information = x.split('(')
-			name = information[0].rstrip().lstrip()
-			name = re.sub(' +',' ',name)
-			universities = information[1].split(';')
-			for x in universities:
-				university = x.split(',')[0].rstrip().lstrip()
-				university = re.sub(' +',' ',university)
-				nu = name + '\t' + university
-				if (nu not in names):
-					names.append(nu)
+   for x in co_organisers:
+      if "Switzerland" in x:
+         information = x.split('(')
+         name = information[0].rstrip().lstrip()
+         name = re.sub(' +',' ',name)
+         information[1] = information[1].replace('Philip Morris International R&D','Philip Morris Int.')
+         if('HES-SO' in information[1]):
+            universities = information[1].split(',')
+         elif ('Netherlands' in information[1]):
+            universities = [information[1].split('&')[0]]
+         else:
+            universities = re.split(';|/|&|,', information[1]);
+         for x in universities:
+            if('Switzerland)' in x):
+               break
+            if('Switzerland' in x):
+               continue
+            university = x.split(',')[0].rstrip().lstrip()
+            university = re.sub(' +',' ',university)
+            university = adjust_university(university)
+            nu = name + '\t' + university
+            if (nu not in names):
+               names.append(nu)
 
-	for count in range(0,len(names)):
-		coauthors = list(names)
-		coauthors.remove(names[count])
-		if(check_if_name_exists(data, names[count])):
-			s = names[count].split('\t')
-			data['authors'].append({'name': s[0], 'university':s[1], 'coauthors': coauthors})
-		else:
-			for c in coauthors:
-				check_if_coauthor_exists_or_add(data, names[count], c)
-
-	return
+   for count in range(0,len(names)):
+      coauthors = list(names)
+      coauthors.remove(names[count])
+      if(check_if_name_exists(data, names[count])):
+         s = names[count].split('\t')
+         data['authors'].append({'name': s[0], 'university':s[1], 'coauthors': coauthors})
+      else:
+         for c in coauthors:
+            check_if_coauthor_exists_or_add(data, names[count], c)
+   return
 
 # Check if the name has been already saved
 def check_if_name_exists(data, nu):
@@ -151,18 +193,19 @@ for y in res:
    parts = filter(None, parts)
    names = []
    for x in parts:
-      y = re.split(',', x)
-      y.remove(y[len(y)-1])
-      university = re.sub(' +',' ',y[len(y)-1]).rstrip().lstrip()
-      y.remove(y[len(y)-1])
+      j = re.split(',', x)
+      j.remove(j[len(j)-1])
+      university = re.sub(' +',' ',j[len(j)-1]).rstrip().lstrip()
+      university = adjust_university(university)
+      j.remove(j[len(j)-1])
 
-      y = ",".join(y)
+      j = ",".join(j)
 
-      y = re.split(', | and', y)
+      j = re.split(', | and', j)
       # y.remove(y[len(y)-1])
 
-      for index in range(0,len(y)):
-         name = re.sub(' +',' ',y[index].replace('and ', ' ')).rstrip().lstrip()
+      for index in range(0,len(j)):
+         name = re.sub(' +',' ',j[index].replace('and ', ' ')).rstrip().lstrip()
          # university = re.sub(' +',' ',x[len(x)-1]).rstrip().lstrip()
          nu = name + '\t' + university
          if (nu not in names):
