92c85e1eefcbed26c066df563c2a35cbd50b7f19
Vanessa Braglia <braglv@usi.ch>
2018-05-04 10:34:59 +0200

    report + graph partitioning

--- data_organization/information_analysis.m
+++ data_organization/information_analysis.m
@@ -4,28 +4,35 @@ TP = pagerank(authors,M,0.85);
 % Display the first 15 auhtors of the ranking
 TP(1:15,:)
 
-% T = T(1:15,:);
-% figure
-% TString = evalc('disp(T)');
-% 
-% % Use TeX Markup for bold formatting and underscores.
-% TString = strrep(TString,'<strong>','\bf');
-% TString = strrep(TString,'</strong>','\rm');
-% TString = strrep(TString,'_','\_');
-% 
-% % Get a fixed-width font.
-% FixedWidth = get(0,'FixedWidthFontName');
-% 
-% % Output the table using the annotation command.
-% annotation(gcf,'Textbox','String',TString,'Interpreter','Tex',...
-%     'FontName',FixedWidth,'Units','Normalized','Position',[0 0 1 1]);
-
 %% Degree Centrality
 % Rank authors only considering only number of collaboration and not
 % quality
 TD = degree_centrality(A,M);
 TD(1:10,:)
 
+
+%% The Reverse Cuthill McKee Ordering
+r = symrcm(M(2:end, 2:end));
+prcm= [1 r+1];
+figure
+subplot(1,2,1)
+spy(M);
+title('Default connectivity matrix')
+subplot(1,2,2)
+spy(M(prcm,prcm))
+title('Ordered connectivity matrix')
+
+% Matrix sparsity = 0.0073
+sparsity = nnz(M)/numel(M);
 %% Spectral graph Partitioning
-TS = spectral_partitioning(M,A);
-TS(1:10,:)
+itr = 0;
+[g1,g2] = spectral_partitioning(M);
+[g3,g4] = spectral_partitioning(M(g1,g1));
+[g5,g6] = spectral_partitioning(M(g2,g2));
+
+group1 = authors(g1(g3));
+group2 = authors(g1(g4));
+group3 = authors(g2(g5));
+group4 = authors(g2(g6));
+
+
--- data_organization/spectral_partitioning.m
+++ data_organization/spectral_partitioning.m
@@ -1,4 +1,5 @@
-function T = spectral_partitioning(M,A)
+function [g1,g2] = spectral_partitioning(M)
+groups = [];
 B = triu(M,1) ;
 B = B + B';
 deg = sum(B);
@@ -6,28 +7,33 @@ L = diag(deg)-B;
 
 [V, ~] =eig(L); 
 [~, p] =sort(V(:,2));
-index1 = p(1:221); 
-index2 = p(221:end);
+% spy(M(p,p))
+% index1 = p(1:221); 
+% index2 = p(221:end);
+s = fix(size(p,1)/2);
+g1 = p(1:s);
+g2 = p(s:end);
 
-size(index1)
-size(index2)
-
-group1=[];
-group2=[];
-
-A = A(1,:);
-for i=1:size(index1,1)
-    group1 = [group1 A(index1(i))];
-end
-
-for i=1:size(index2,1)
-    group2 = [group2 A(index2(i))];
-end
-
-size(group1)
-size(group2)
-
-T = table(index1, group1', index2, group2');
-T.Properties.VariableNames{'Var2'} = 'Group1';
-T.Properties.VariableNames{'Var4'} = 'Group2';
+% size(index1)
+% size(index2)
+% 
+% group1=[];
+% group2=[];
+% 
+% A = A(1,:);
+% for i=1:size(index1,1)
+%     group1 = [group1 A(index1(i))];
+% end
+% 
+% for i=1:size(index2,1)
+%     group2 = [group2 A(index2(i))];
+% end
+% 
+% size(group1)
+% size(group2)
+% group1{1}
+% group2{1}
+% T = table(index1, group1', index2, group2');
+% T.Properties.VariableNames{'Var2'} = 'Group1';
+% T.Properties.VariableNames{'Var4'} = 'Group2';
 end
